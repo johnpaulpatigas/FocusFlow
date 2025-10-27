c9e17edcba1875f74ab5e704b3ec1497d34047aa
johnpaulpatigas <johnpaulpatigas@gmail.com>
2025-10-27 17:26:24 +0800

    frontend: feat: Add Android platform and Capacitor-specific features

---
+++ frontend/android/app/src/main/java/com/johnpaulpatigas/focusflow/MainActivity.java
@@ -0,0 +1,5 @@
+package com.johnpaulpatigas.focusflow;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import { Capacitor } from "@capacitor/core";
+import { StatusBar, Style } from "@capacitor/status-bar";
+import { EdgeToEdge } from "@capawesome/capacitor-android-edge-to-edge-support";
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 
 function App() {
+  useEffect(() => {
+    if (Capacitor.isNativePlatform()) {
+      EdgeToEdge.setBackgroundColor({ color: "#0f172b" });
+      StatusBar.setBackgroundColor({ color: "#0f172b" });
+      StatusBar.setStyle({ style: Style.Dark });
+    }
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
