29ee6b8fdfbbf83ab4745c16cb7857c8b2a1718c
johnpaulpatigas <johnpaulpatigas@gmail.com>
2025-12-03 14:13:31 +0800

    frontend: refactor: Use environment variable for API base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,10 @@
 // src/api/axios.js
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 const apiClient = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: baseURL,
 });
 
 apiClient.interceptors.request.use(
